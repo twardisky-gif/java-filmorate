cef5db7cd6e3a78059db2b5049e1f9c09c778150
twardisky-gif <298361131+twardisky-gif@users.noreply.github.com>
2026-07-31 14:29:24 +0300

    refactor: выделить интерфейсы сервисного слоя
    Добавить интерфейсы FilmService и UserService, реализации вынести в
    FilmServiceImpl и UserServiceImpl для расширяемости приложения.

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,85 +1,22 @@
 package ru.yandex.practicum.filmorate.service;
 
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
-@Slf4j
-@Service
-public class FilmService {
-    private static final LocalDate CINEMA_BIRTHDAY = LocalDate.of(1895, 12, 28);
-    private static final int DEFAULT_POPULAR_COUNT = 10;
-    private final FilmStorage filmStorage;
-    private final UserService userService;
+public interface FilmService {
+    Film create(Film film);
 
-    public FilmService(FilmStorage filmStorage, UserService userService) {
-        this.filmStorage = filmStorage;
-        this.userService = userService;
-    }
+    Film update(Film film);
 
-    public Film create(Film film) {
-        validateReleaseDate(film);
-        Film created = filmStorage.add(film);
-        log.info("Добавлен фильм: {}", created.getName());
-        return created;
-    }
+    Film getById(long id);
 
-    public Film update(Film film) {
-        getFilmOrThrow(film.getId());
-        validateReleaseDate(film);
-        Film updated = filmStorage.update(film);
-        log.info("Обновлен фильм: {}", updated.getName());
-        return updated;
-    }
+    Collection<Film> getAll();
 
-    public Film getById(long id) {
-        return getFilmOrThrow(id);
-    }
+    void addLike(long filmId, long userId);
 
-    public Collection<Film> getAll() {
-        return filmStorage.getAll();
-    }
+    void removeLike(long filmId, long userId);
 
-    public void addLike(long filmId, long userId) {
-        Film film = getFilmOrThrow(filmId);
-        userService.getById(userId);
-        film.getLikes().add(userId);
-        log.info("Пользователь {} поставил лайк фильму {}", userId, filmId);
-    }
-
-    public void removeLike(long filmId, long userId) {
-        Film film = getFilmOrThrow(filmId);
-        userService.getById(userId);
-        film.getLikes().remove(userId);
-        log.info("Пользователь {} удалил лайк фильму {}", userId, filmId);
-    }
-
-    public List<Film> getPopular(int count) {
-        int limit = count > 0 ? count : DEFAULT_POPULAR_COUNT;
-        return filmStorage.getAll().stream()
-                .sorted((f1, f2) -> Integer.compare(f2.getLikes().size(), f1.getLikes().size()))
-                .limit(limit)
-                .collect(Collectors.toList());
-    }
-
-    private void validateReleaseDate(Film film) {
-        LocalDate releaseDate = film.getReleaseDate();
-        if (releaseDate != null && releaseDate.isBefore(CINEMA_BIRTHDAY)) {
-            log.warn("Дата релиза раньше 28 декабря 1895 года");
-            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
-        }
-    }
-
-    private Film getFilmOrThrow(long id) {
-        return filmStorage.getById(id)
-                .orElseThrow(() -> new NotFoundException("Фильм с id=" + id + " не найден"));
-    }
+    List<Film> getPopular(int count);
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -2,90 +2,23 @@ package ru.yandex.practicum.filmorate.service;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
-@Slf4j
-@Service
-public class UserService {
-    private final UserStorage userStorage;
+public interface UserService {
+    User create(User user);
 
-    public UserService(UserStorage userStorage) {
-        this.userStorage = userStorage;
-    }
+    User update(User user);
 
-    public User create(User user) {
-        applyNameIfEmpty(user);
-        User created = userStorage.add(user);
-        log.info("Создан пользователь: {}", created.getLogin());
-        return created;
-    }
+    User getById(long id);
 
-    public User update(User user) {
-        getUserOrThrow(user.getId());
-        applyNameIfEmpty(user);
-        User updated = userStorage.update(user);
-        log.info("Обновлен пользователь: {}", updated.getLogin());
-        return updated;
-    }
+    Collection<User> getAll();
 
-    public User getById(long id) {
-        return getUserOrThrow(id);
-    }
+    void addFriend(long userId, long friendId);
 
-    public Collection<User> getAll() {
-        return userStorage.getAll();
-    }
+    void removeFriend(long userId, long friendId);
 
-    public void addFriend(long userId, long friendId) {
-        User user = getUserOrThrow(userId);
-        User friend = getUserOrThrow(friendId);
-        user.getFriends().add(friendId);
-        friend.getFriends().add(userId);
-        log.info("Пользователи {} и {} стали друзьями", userId, friendId);
-    }
+    List<User> getFriends(long userId);
 
-    public void removeFriend(long userId, long friendId) {
-        User user = getUserOrThrow(userId);
-        User friend = getUserOrThrow(friendId);
-        user.getFriends().remove(friendId);
-        friend.getFriends().remove(userId);
-        log.info("Пользователи {} и {} больше не друзья", userId, friendId);
-    }
-
-    public List<User> getFriends(long userId) {
-        User user = getUserOrThrow(userId);
-        return user.getFriends().stream()
-                .map(this::getUserOrThrow)
-                .collect(Collectors.toList());
-    }
-
-    public List<User> getCommonFriends(long userId, long otherId) {
-        User user = getUserOrThrow(userId);
-        User other = getUserOrThrow(otherId);
-        Set<Long> userFriends = user.getFriends();
-        Set<Long> otherFriends = other.getFriends();
-        return userFriends.stream()
-                .filter(otherFriends::contains)
-                .map(this::getUserOrThrow)
-                .collect(Collectors.toList());
-    }
-
-    private void applyNameIfEmpty(User user) {
-        String name = user.getName();
-        if (name == null || name.isBlank()) {
-            user.setName(user.getLogin());
-        }
-    }
-
-    private User getUserOrThrow(long id) {
-        return userStorage.getById(id)
-                .orElseThrow(() -> new NotFoundException("Пользователь с id=" + id + " не найден"));
-    }
+    List<User> getCommonFriends(long userId, long otherId);
 }
