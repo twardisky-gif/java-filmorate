3a4cf406d2382e0274f272531d14f2ca4a47f63f
twardisky-gif <twardisky@gmail.com>
2026-08-02 03:29:43 +0300

    Update UserController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,64 +3,76 @@ package ru.yandex.practicum.filmorate.controller;
 import java.util.Collection;
 import java.util.List;
 
-import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.service.UserService;
 
-@RestController
-@RequestMapping("/users")
-public class UserController {
-    private final UserService userService;
+/**
+ * Операции над пользователями и их друзьями.
+ */
+public interface UserController {
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+    /**
+     * Возвращает всех пользователей.
+     *
+     * @return коллекция пользователей
+     */
+    Collection<User> getAll();
 
-    @GetMapping
-    public Collection<User> getAll() {
-        return userService.getAll();
-    }
+    /**
+     * Возвращает пользователя по идентификатору.
+     *
+     * @param id идентификатор пользователя
+     * @return найденный пользователь
+     * @throws ru.yandex.practicum.filmorate.exception.NotFoundException если пользователь не найден
+     */
+    User getById(long id);
 
-    @GetMapping("/{id}")
-    public User getById(@PathVariable long id) {
-        return userService.getById(id);
-    }
+    /**
+     * Создаёт нового пользователя.
+     *
+     * @param user данные пользователя
+     * @return сохранённый пользователь с присвоенным идентификатором
+     */
+    User create(User user);
 
-    @PostMapping
-    public User create(@Valid @RequestBody User user) {
-        return userService.create(user);
-    }
+    /**
+     * Обновляет существующего пользователя.
+     *
+     * @param user данные пользователя с заполненным идентификатором
+     * @return обновлённый пользователь
+     * @throws ru.yandex.practicum.filmorate.exception.NotFoundException если пользователь не найден
+     */
+    User update(User user);
 
-    @PutMapping
-    public User update(@Valid @RequestBody User user) {
-        return userService.update(user);
-    }
+    /**
+     * Добавляет пользователя в друзья. Дружба односторонняя.
+     *
+     * @param id идентификатор пользователя
+     * @param friendId идентификатор добавляемого друга
+     */
+    void addFriend(long id, long friendId);
 
-    @PutMapping("/{id}/friends/{friendId}")
-    public void addFriend(@PathVariable long id, @PathVariable long friendId) {
-        userService.addFriend(id, friendId);
-    }
+    /**
+     * Удаляет пользователя из друзей.
+     *
+     * @param id идентификатор пользователя
+     * @param friendId идентификатор удаляемого друга
+     */
+    void removeFriend(long id, long friendId);
 
-    @DeleteMapping("/{id}/friends/{friendId}")
-    public void removeFriend(@PathVariable long id, @PathVariable long friendId) {
-        userService.removeFriend(id, friendId);
-    }
+    /**
+     * Возвращает друзей пользователя.
+     *
+     * @param id идентификатор пользователя
+     * @return список друзей
+     */
+    List<User> getFriends(long id);
 
-    @GetMapping("/{id}/friends")
-    public List<User> getFriends(@PathVariable long id) {
-        return userService.getFriends(id);
-    }
-
-    @GetMapping("/{id}/friends/common/{otherId}")
-    public List<User> getCommonFriends(@PathVariable long id, @PathVariable long otherId) {
-        return userService.getCommonFriends(id, otherId);
-    }
+    /**
+     * Возвращает общих друзей двух пользователей.
+     *
+     * @param id идентификатор первого пользователя
+     * @param otherId идентификатор второго пользователя
+     * @return список общих друзей
+     */
+    List<User> getCommonFriends(long id, long otherId);
 }
