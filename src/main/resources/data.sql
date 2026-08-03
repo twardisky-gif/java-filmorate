a520320f18b24918da048dc60736d571648b3a1c
Vasili Usoltsev <vasili.n.usoltsev@yandex.com>
2026-08-04 02:08:26 +0700

    3-ая итерация функциональности - вывод самых популярных фильмов по жанру и годам - сформировал набор данных ждя теста

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -23,15 +23,15 @@ MERGE INTO genres (genre_id, name) KEY (genre_id) VALUES
 (5, 'Документальный'),
 (6, 'Боевик');
 
-INSERT INTO users (user_id, email, login, name, birthday) VALUES
-(1, 'user1@mail.ru', 'user1', 'User One', '1990-01-01'),
-(2, 'user2@mail.ru', 'user2', 'User Two', '1995-05-05'),
-(3, 'user3@mail.ru', 'user3', 'User Three', '2000-10-10');
+INSERT INTO users (email, login, name, birthday) VALUES
+('user1@mail.ru', 'user1', 'User One', '1990-01-01'),
+('user2@mail.ru', 'user2', 'User Two', '1995-05-05'),
+('user3@mail.ru', 'user3', 'User Three', '2000-10-10');
 
-INSERT INTO films (film_id, name, description, release_date, duration, mpa_id) VALUES
-(1, 'Old Film', 'Old film description', '1989-04-17', 190, 5),
-(2, 'New film', 'New film about friends', '1999-04-30', 120, 3),
-(3, 'New film with director', 'Film with director', '1999-04-30', 120, 3);
+INSERT INTO films (name, description, release_date, duration, mpa_id) VALUES
+('Old Film', 'Old film description', '1989-04-17', 190, 5),
+('New film', 'New film about friends', '1999-04-30', 120, 3),
+('New film with director', 'Film with director', '1999-04-30', 120, 3);
 
 INSERT INTO film_genres (film_id, genre_id) VALUES
 (1, 1),
