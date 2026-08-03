5b7f7363ca18e4b4c63c0369f751de16faec51a8
Vasili Usoltsev <vasili.n.usoltsev@yandex.com>
2026-08-04 02:20:08 +0700

    3-ая итерация функциональности - вывод самых популярных фильмов по жанру и годам - откатил генерируемые данные и схему БД

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,47 +1,6 @@
-DELETE FROM film_genres;
-DELETE FROM likes;
-DELETE FROM films;
-DELETE FROM users;
-DELETE FROM genres;
-DELETE FROM mpa;
+MERGE INTO mpa (mpa_id, name) KEY (mpa_id)
+VALUES (1, 'G'), (2, 'PG'), (3, 'PG-13'), (4, 'R'), (5, 'NC-17');
 
-ALTER TABLE films ALTER COLUMN film_id RESTART WITH 1;
-ALTER TABLE users ALTER COLUMN user_id RESTART WITH 1;
-
-MERGE INTO mpa (mpa_id, name) KEY (mpa_id) VALUES
-(1, 'G'),
-(2, 'PG'),
-(3, 'PG-13'),
-(4, 'R'),
-(5, 'NC-17');
-
-MERGE INTO genres (genre_id, name) KEY (genre_id) VALUES
-(1, 'Комедия'),
-(2, 'Драма'),
-(3, 'Мультфильм'),
-(4, 'Триллер'),
-(5, 'Документальный'),
-(6, 'Боевик');
-
-INSERT INTO users (email, login, name, birthday) VALUES
-('user1@mail.ru', 'user1', 'User One', '1990-01-01'),
-('user2@mail.ru', 'user2', 'User Two', '1995-05-05'),
-('user3@mail.ru', 'user3', 'User Three', '2000-10-10');
-
-INSERT INTO films (name, description, release_date, duration, mpa_id) VALUES
-('Old Film', 'Old film description', '1989-04-17', 190, 5),
-('New film', 'New film about friends', '1999-04-30', 120, 3),
-('New film with director', 'Film with director', '1999-04-30', 120, 3);
-
-INSERT INTO film_genres (film_id, genre_id) VALUES
-(1, 1),
-(2, 1),
-(2, 2),
-(3, 1);
-
-INSERT INTO likes (film_id, user_id) VALUES
-(2, 1),
-(2, 2),
-(2, 3),
-(3, 1),
-(3, 2);
+MERGE INTO genres (genre_id, name) KEY (genre_id)
+VALUES (1, 'Комедия'), (2, 'Драма'), (3, 'Мультфильм'),
+       (4, 'Триллер'), (5, 'Документальный'), (6, 'Боевик');
